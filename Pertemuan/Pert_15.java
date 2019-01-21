ea744417c2466e1b178759f63016dfa3d7fe5289
alvinvin00 <44573205+alvinvin00@users.noreply.github.com>
2019-01-21 18:50:41 +0700

    Update Pert_15.java

--- Pertemuan/Pert_15.java
+++ Pertemuan/Pert_15.java
@@ -3,14 +3,6 @@ package testpack;
 public class Pert_15 {
 
 	public static void main(String[] args) {
-		// int b = 6;
-		// for (int i = 6; i > 0; i--) {
-		// System.out.print(i + " ");
-		// if (i == 1) {
-		// i = b;
-		// b--;
-		// }
-		// }
 		for (int i = 1; i <= 8; i++) { // Baris
 			for (int j = 1; j <= i; j++) { // Kolom
 				System.out.print(i+" ");
